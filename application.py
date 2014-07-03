4d94cce8684253139d12e90f13fdcd0dc4b1da39
Adam N. Morgan <qmorgan@gmail.com>
2014-07-03 08:40:35 -0700

    Attempted to add a connection test

--- application.py
+++ application.py
@@ -50,6 +50,27 @@ try:
 except:
     print "Cannot connect!"
 
+
+
+from sqlalchemy import exc
+from sqlalchemy import event
+from sqlalchemy.pool import Pool
+
+@event.listens_for(Pool, "checkout")
+def ping_connection(dbapi_connection, connection_record, connection_proxy):
+    cursor = dbapi_connection.cursor()
+    try:
+        cursor.execute("SELECT 1")
+    except:
+        # optional - dispose the whole pool
+        # instead of invalidating one at a time
+        # connection_proxy._pool.dispose()
+
+        # raise DisconnectionError - pool will try
+        # connecting again up to three times before raising.
+        raise exc.DisconnectionError()
+    cursor.close()
+
 @app.route("/")
 def index():
     # right now just force them to go to search
